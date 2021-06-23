12f605baefa4ee0d5ba0e1e4d4c97258033c9104
illusionoff <venom12@inbox.ru>
2021-06-23 18:48:01 +0300

    init project

---
+++ functions/functions.js
@@ -0,0 +1,220 @@
+
+const config = require('config');
+const MIN_PROFIT = config.get('MIN_PROFIT');
+const stringify = require('csv-stringify');
+const generate = require('csv-generate');
+const assert = require('assert');
+const fs = require("fs");
+
+function goTrade(paramsGoTrade, writeableStream, counts) {
+
+  // let counts = {
+  //   count: 0,
+  //   countMessageAll: 0,
+  //   countMessageStartNew: 0
+  // }
+
+  let diffSell = paramsGoTrade.bayBith - paramsGoTrade.sellGate;
+  let diffBay = paramsGoTrade.bayGate - paramsGoTrade.sellBith;
+
+  if (diffSell > 0) {
+    console.log('Выгодно купить на Gate и продать на Bith = #1');
+    const percentBonus = diffSell / paramsGoTrade.sellGate;
+    console.log('percentBonus #1 =', percentBonus);
+  }
+
+  if (diffBay > 0) {
+    console.log('Выгодно продать на Gate и купить на Bith = #2');
+    const percentBonus = diffBay / paramsGoTrade.sellBith;
+    console.log('percentBonus #2=', percentBonus);
+  }
+  console.log('diffSell=', diffSell);
+  console.log('diffBay=', diffBay);
+
+  // if (diffSell > 0 || diffBay > 0) {
+  const data = {
+    bayGate: paramsGoTrade.bayGate,
+    bayBith: paramsGoTrade.bayBith,
+    sellGate: paramsGoTrade.sellGate,
+    sellBith: paramsGoTrade.sellBith,
+    diffSell: diffSell,
+    diffBay: diffBay,
+    timeServer: paramsGoTrade.timeServer,
+    timeBith: paramsGoTrade.timeBith,
+    init: paramsGoTrade.init
+  }
+  writtenCSV(data, writeableStream, counts);
+  // }
+
+  if ((diffSell > config.get("MIN_PROFIT") || diffBay > config.get("MIN_PROFIT"))) {
+
+  }
+};
+
+
+function writtenCSV(data, writeableStream, counts) {
+  counts.countMessageAll++;
+  counts.countMessageStartNew++;
+  // console.log('writeableStream._writableState:', writeableStream._writableState);
+  // console.log('writeableStream:', writeableStream);
+  // console.log('writeableStream._writableState.closed', writeableStream._writableState.closed);
+  console.log('writeableStream.closed', writeableStream.closed);
+  // console.log('writeableStream.path:', writeableStream.path);
+  console.log('writeableStream._writableState.finished:', writeableStream._writableState.finished);
+  console.log('writeableStream._writableState.ended:', writeableStream._writableState.ended);
+  // if (ended)
+  // console.log('writeableStream:', writeableStream);
+  // if (!writeableStream._writableState.ended || !writeableStream._writableState.finished || !writeableStream._writableState.closed) {
+
+
+  // var tempFile = fs.createWriteStream(tempFilepath);
+  // tempFile.on('open', function(fd) {
+  //     http.request(url, function(res) {
+  //         res.on('data', function(chunk) {
+  //             tempFile.write(chunk);
+  //         }).on('end', function() {
+  //             tempFile.end();
+  //             fs.renameSync(tempFile.path, filepath);
+  //             return callback(filepath);
+  //         });
+  //     });
+  // });
+
+  // tempFile.on('open', function(fd) {
+
+  if (!writeableStream._writableState.ended || !writeableStream._writableState.finished || !writeableStream.closed) {
+
+    let time = new Date().getTime();
+    console.log('time:', time);
+    console.log('counts.countMessageStartNew 1:', counts.countMessageStartNew);
+
+    const stringifyDate = stringify([
+      data
+    ], {
+      columns: ['timeServer', 'timeBith', 'init', 'bayGate', 'bayBith', 'sellGate', 'sellBith', 'diffSell', 'diffBay']
+      // timeServer,timeBith,init,bayGate,bayBith,sellGate,sellBith,diffSell,diffBay
+    }, function (err, data) {
+      // assert.equal(
+      //   data,
+      //   "XXX XXXX,XXXX,\n" +
+      //   "YYY YYYY,YYYY,\n"
+      // )
+      console.log('data=', data);
+      // writeableStream.write(`${data}\r\n`);
+      writeableStream.write(data);
+      // }).pipe(writeableStream);
+    });
+
+    // writeableStream.write(`stringifyDate_${stringifyDate}\r\n`);
+    // writeableStream.write(`writeableStream_${countMessageAll}_size_${fileSizeInBytes}\r\n`);
+
+    // writeableStream.write(`writeableStream_${counts.countMessageStartNew}\r\n`);
+
+    // let stats = fs.stat("logs/profit.csv", (error, stats) => {
+    //   if (error) {
+    //     console.log(error);
+    //   }
+    //   else {
+    //     // console.log("Stats object for: profit.csv");
+    //     // console.log(stats);
+
+    //     // Using methods of the Stats object
+    //     // console.log("Path is file:", stats.isFile());
+    //     // console.log("Path is directory:", stats.isDirectory());
+    //     let fileSizeInBytes = stats["size"];
+    //     console.log('fileSizeInBytes=', fileSizeInBytes);
+    //     writeableStream.write(`writeableStream_${countMessageAll}_size_${fileSizeInBytes}\r\n`);
+    //   }
+    // });
+
+
+  } else {
+    counts.count++;
+    console.log('if writeableStream._writableState.ended2:', writeableStream._writableState.ended);
+    console.log(' writeableStream._writableState.ended2 count :', counts.count);
+    console.log(' File not writting !!!');
+  }
+  // var stats = fs.statSync("myfile.txt")
+  // const fileSizeInBytes = stats["size"];
+  // //Convert the file size to megabytes (optional)
+  // // const fileSizeInMegabytes = fileSizeInBytes / 1000000.0;
+  // // console.log('fileSizeInMegabytes:', fileSizeInMegabytes);
+  // console.log('fileSizeInBytes:', fileSizeInBytes);
+  console.log('counts.countMessageAll 2=', counts.countMessageAll);
+  if ((counts.countMessageAll) > 10) {
+    console.log(' countMessage  if ((countMessage) > 20:', counts.countMessageAll);
+    counts.countMessageAll = 0;
+    writeableStream.end();
+    // writeableStream.close();
+
+    // if (writeableStream._writableState.closed) {
+    let time = new Date().getTime();
+    console.log('time:', time);
+
+    console.log('1----writeableStream.closed', writeableStream.closed);
+    // console.log('writeableStream.path:', writeableStream.path);
+    console.log('1----writeableStream._writableState.finished:', writeableStream._writableState.finished);
+    console.log('1----writeableStream._writableState.ended:', writeableStream._writableState.ended);
+    writeableStream = fs.createWriteStream(`logs/profit${time}.csv`, { flags: 'a' });
+
+    console.log('2----writeableStream.closed', writeableStream.closed);
+    // console.log('writeableStream.path:', writeableStream.path);
+    console.log('2----writeableStream._writableState.finished:', writeableStream._writableState.finished);
+    console.log('2----writeableStream._writableState.ended:', writeableStream._writableState.ended);
+    // }
+
+  }
+  // let data = data;
+  // { year: 'XXXX', phone: 'XXX XXXX', nocolumn: 'XXX1' },
+  // { year: 'YYYY', phone: 'YYY YYYY', nocolumn: 'YYY1' }
+
+
+
+
+  // columns: ['bayGate', 'bayBith', 'sellGate', 'sellBith', 'timestampServer']
+
+  // const i = setInterval(() => {
+  //   writeableStream.write(JSON.stringify(writeToFile), (err) => console.log(`wrote ${JSON.stringify(writeToFile)} to file`))
+  // }, 1000)
+
+
+  // let timerId = setInterval(() => stringify([
+  //   // { year: 'XXXX', phone: 'XXX XXXX', nocolumn: 'XXX1', timestamp: new Date() },
+  //   // { year: 'YYYY', phone: 'YYY YYYY', nocolumn: 'YYY1', timestamp: new Date() }
+  //   { year: 'XXXX', phone: 'XXX XXXX', nocolumn: 'XXX1', timestamp: new Date() },
+  //   { year: 'YYYY', phone: 'YYY YYYY', nocolumn: 'YYY1', timestamp: new Date() }
+  // ], {
+  //   columns: ['phone', 'year', 'nocolumn', 'timestamp']
+  // }, function (err, data) {
+  //   writeableStream.write(data, (err) => console.log(`wrote ${data} to file`))
+  //   // assert.equal(
+  //   //   data,
+  //   //   "XXX XXXX,XXXX,\n" +
+  //   //   "YYY YYYY,YYYY,\n"
+  //   // )
+  //   console.log('data=', data);
+  // }), 2000);
+
+  // let timerId = setInterval(() => stringify([
+  //   { year: 'XXXX', phone: 'XXX XXXX', nocolumn: 'XXX1' },
+  //   { year: 'YYYY', phone: 'YYY YYYY', nocolumn: 'YYY1' }
+  // ], {
+  //   columns: ['phone', 'year', 'nocolumn']
+  // }, function (err, data) {
+  //   // assert.equal(
+  //   //   data,
+  //   //   "XXX XXXX,XXXX,\n" +
+  //   //   "YYY YYYY,YYYY,\n"
+  //   // )
+  //   console.log('data=', data);
+  // }).pipe(writeableStream), 2000);
+
+
+  writeableStream.on('open', () => {
+    console.log('open File !!!');
+  });
+  writeableStream.once('finish', () => {
+    console.log('wrote all data to file');
+  });
+}
+module.exports = { goTrade, writtenCSV }
