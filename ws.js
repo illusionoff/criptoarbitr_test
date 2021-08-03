a74be3fe33746e4ab740ff99c3da7bafc0728f09
MiniPC <venom12@inbox.ru>
2021-08-03 13:27:11 +0300

    Test bith correctTimeServerClosure 4.1

--- ws.js
+++ ws.js
@@ -54,21 +54,21 @@ function init() {
   let writableFiles = TestWritable();
   // writableFiles('rrrr'); //работает ередача аргументов в замыкание!!!
   coinConfigBith();
-  // wsStartBith('subscribe', "TRADE:XRP-USDT", initialGate, counts, writeableStream); // была проблема с самим сервером Bithump, не отсылал сообщения 00007, поэтому делал альтернативный вариант
+  //// wsStartBith('subscribe', "TRADE:XRP-USDT", initialGate, counts, writeableStream); // была проблема с самим сервером Bithump, не отсылал сообщения 00007, поэтому делал альтернативный вариант
 
-  wsStartBith('subscribe', "ORDERBOOK:XRP-USDT", initialGate, writableFiles);/////////////////////////
+  // wsStartBith('subscribe', "ORDERBOOK:XRP-USDT", initialGate, writableFiles);/////////////////////////
 
   // wsStartBithOrder10('subscribe', "ORDERBOOK10:XRP-USDT", initialGate, writableFiles);/////////////////////////////// не получилось использовать из-за нестабильности ответов 00007
-  // wsGetGate(Math.round(Math.random() * 1000), 'depth.subscribe', ["XRP_USDT", 10, "0.0001"], initialBith, writableFiles); // передаем данные для сравнения из bithumb
+  //// wsGetGate(Math.round(Math.random() * 1000), 'depth.subscribe', ["XRP_USDT", 10, "0.0001"], initialBith, writableFiles); // передаем данные для сравнения из bithumb
 
-  // wsGetGate(Number(new Date().getTime()), 'spot.book_ticker', 'subscribe', ["XRP_USDT"], initialBith, writableFiles); // передаем данные для сравнения из bithumb//////////
+  //// wsGetGate(Number(new Date().getTime()), 'spot.book_ticker', 'subscribe', ["XRP_USDT"], initialBith, writableFiles); // передаем данные для сравнения из bithumb//////////
 
-  // wsGetGate(Number(new Date().getTime()), 'spot.order_book', 'subscribe', ["XRP_USDT", "10", "100ms"], initialBith, writableFiles); // передаем данные для /////////
+  wsGetGate(Number(new Date().getTime()), 'spot.order_book', 'subscribe', ["XRP_USDT", "10", "100ms"], initialBith, writableFiles); // передаем данные для /////////
 
-  // gate.wsGet(Math.round(Math.random()*1000),'depth.unsubscribe', []);
-  // gate.wsGet(Math.round(Math.random()*1000),'kline.query', ["BTC_USDT", 1, 1516951219, 1800]);
-  // gate.wsGet(Math.round(Math.random()*1000),'kline.subscribe', ["BTC_USDT", 1800]);
-  // gate.wsGet(Math.round(Math.random()*1000),'kline.unsubscribe', []);
+  //// gate.wsGet(Math.round(Math.random()*1000),'depth.unsubscribe', []);
+  //// gate.wsGet(Math.round(Math.random()*1000),'kline.query', ["BTC_USDT", 1, 1516951219, 1800]);
+  //// gate.wsGet(Math.round(Math.random()*1000),'kline.subscribe', ["BTC_USDT", 1800]);
+  //// gate.wsGet(Math.round(Math.random()*1000),'kline.unsubscribe', []);
 
   // gate.wsGet(Math.round(Math.random()*1000),'kline.update', [ 1492358400, "7000.00","8000.0","8100.00","6800.00", "1000.00","123456.00","BTC_USDT"]);
 
