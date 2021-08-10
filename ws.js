3d274845d2cc6c78fad792399ec7a9f51f49fadb
MiniPC <venom12@inbox.ru>
2021-08-10 16:11:37 +0300

    changeFirstOrderbook() 2.2

--- ws.js
+++ ws.js
@@ -1,14 +1,15 @@
 // const fs = require("fs");
 
 const { wsGetGate, initialGate } = require('./lib/gate');
-const { wsStartBith, initialBith } = require('./lib/bithumbpro');//coinConfigBith
+const { wsStartBith, initialBith, coinConfigBith } = require('./lib/bithumbpro');//coinConfigBith
 const { writtenCSV, TestWritable, parseCSV } = require('./functions/functions');
-const { wsStartBithOder10, coinConfigBith } = require('./lib/bithOrderbook10');
+// const { coinConfigBith } = require('./lib/bithOrderbook');
 
 function init() {
   parseCSV();
   let writableFiles = TestWritable();
   coinConfigBith();
+  // process.exit();
   //// wsStartBith('subscribe', "TRADE:XRP-USDT", initialGate, counts, writeableStream); // была проблема с самим сервером Bithump, не отсылал сообщения 00007, поэтому делал альтернативный вариант
   wsStartBith('subscribe', "ORDERBOOK:XRP-USDT", initialGate, writableFiles);/////////////////////////
   // wsGetGate(Number(new Date().getTime()), 'spot.order_book', 'subscribe', ["XRP_USDT", "10", "100ms"], initialBith, writableFiles); // передаем данные для /////////
