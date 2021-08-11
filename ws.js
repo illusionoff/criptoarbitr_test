3211c63a955560d0d9c5850960d10e223afb14c2
MiniPC <venom12@inbox.ru>
2021-08-11 13:09:51 +0300

    Bith and Gate ws. not worcing double. Needed testing on PC and server

--- ws.js
+++ ws.js
@@ -8,30 +8,17 @@ const { writtenCSV, TestWritable, parseCSV } = require('./functions/functions');
 function init() {
   parseCSV();
   let writableFiles = TestWritable();
-  // coinConfigBith().then((result) => {
-  //   // console.log('result=', result);
-  //   if (result) {
-  //     //// wsStartBith('subscribe', "TRADE:XRP-USDT", initialGate, counts, writeableStream); // была проблема с самим сервером Bithump, не отсылал сообщения 00007, поэтому делал альтернативный вариант
-  //     wsStartBith('subscribe', "ORDERBOOK:XRP-USDT", initialGate, writableFiles);/////////////////////////
-  //     // wsGetGate(Number(new Date().getTime()), 'spot.order_book', 'subscribe', ["XRP_USDT", "10", "100ms"], initialBith, writableFiles); // передаем данные для /////////
-  //   } else {
-  //     init();
-  //   }
-
-  // });  
   coinConfigBith().then(() => {
     console.log('then=');
     //// wsStartBith('subscribe', "TRADE:XRP-USDT", initialGate, counts, writeableStream); // была проблема с самим сервером Bithump, не отсылал сообщения 00007, поэтому делал альтернативный вариант
     wsStartBith('subscribe', "ORDERBOOK:XRP-USDT", initialGate, writableFiles);/////////////////////////
-    // wsGetGate(Number(new Date().getTime()), 'spot.order_book', 'subscribe', ["XRP_USDT", "10", "100ms"], initialBith, writableFiles); // передаем данные для /////////
-
+    wsGetGate(Number(new Date().getTime()), 'spot.order_book', 'subscribe', ["XRP_USDT", "10", "100ms"], initialBith, writableFiles); // передаем данные для /////////
   })
     .catch((err) => {
       console.log('catch');
       console.log('err=', err);
       init();
     });
-
 }
 
 init();
